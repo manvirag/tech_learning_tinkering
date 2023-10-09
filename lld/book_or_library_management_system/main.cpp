5a9cce218f378d55c5ee774dde58b4c2d5278457
manvirag <anubhavguptadec1999@gmail.com>
2023-10-09 10:13:41 +0530

    feat: add the basic design of book management system

---
+++ lld/book_or_library_management_system/main.cpp
@@ -0,0 +1,9 @@
+/*
+Ref -> https://workat.tech/machine-coding/practice/design-library-management-system-jgjrv8q8b136
+
+design link
+
+https://anubhavgupta606417.invisionapp.com/freehand/Book-Management-6UfUYoiCs
+
+
+*/
