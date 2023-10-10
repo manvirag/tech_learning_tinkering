848563da1e126d0fcae7f6d5fbb0d23dfaed82a9
manvirag <anubhavguptadec1999@gmail.com>
2023-10-10 23:46:40 +0530

    feat: added the design link of remaining problems

---
+++ lld/parkinglot/main.cpp
@@ -0,0 +1,7 @@
+/*
+Ref: https://www.youtube.com/watch?v=7IX84K9g23U&t=1161s
+
+design link:
+https://anubhavgupta606417.invisionapp.com/freehand/Parking-Lot-Lld-JC3wOdK2o
+
+*/
---
+++ lld/ticket_booking_system/main.cpp
@@ -0,0 +1,8 @@
+/*
+
+ Ref: https://github.com/anomaly2104/ticket-booking-low-level-system-design
+ 
+ design link: https://anubhavgupta606417.invisionapp.com/freehand/Ticket-ManageMent-LLd-n1lNLeU7I
+
+
+*/
