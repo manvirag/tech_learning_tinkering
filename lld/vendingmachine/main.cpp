08c4ac449a4496229cdc18fec2a410adf39c7a2b
manvirag <anubhavguptadec1999@gmail.com>
2023-10-10 22:21:35 +0530

    feat: vending machine design link

---
+++ lld/vendingmachine/main.cpp
@@ -0,0 +1,8 @@
+/*
+Ref -> https://www.youtube.com/watch?v=wOXs5Z_z0Ew&t=593s and https://gitlab.com/shrayansh8/interviewcodingpractise/-/tree/main/src/LowLevelDesign/DesignVendingMachine
+
+design link
+
+https://anubhavgupta606417.invisionapp.com/freehand/Vending-Machine-ne6y6Q2Hx
+
+*/
