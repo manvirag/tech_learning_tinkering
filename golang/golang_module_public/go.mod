f0767b081e460c27eb134858c6cf66a17394529c
manvirag <anubhavguptadec1999@gmail.com>
2023-10-29 22:30:56 +0530

    feat: sample public repo

---
+++ golang/golang_module_public/go.mod
@@ -0,0 +1,3 @@
+module github.com/manvirag982/tech_learning_tinkering/tree/main/golang/golang_module_public
+
+go 1.20
---
+++ golang/golang_module_public/public_module.go
@@ -0,0 +1,7 @@
+package golangmodulepublic
+
+import "fmt"
+
+func HelloWorld() {
+	fmt.Println("I am from the public repo. I am for testing i will be using in another project")
+}
