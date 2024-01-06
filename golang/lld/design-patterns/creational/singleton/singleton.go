d612c35c43337bc8e0b8ed4d2aab4859a9e1d800
manvirag <anubhavguptadec1999@gmail.com>
2024-01-06 12:04:34 +0530

    fix: singleton design pattern concurrency warning added

--- golang/lld/design-patterns/creational/singleton/singleton.go
+++ golang/lld/design-patterns/creational/singleton/singleton.go
@@ -1,5 +1,45 @@
 package main
 
+/*
+it's important to note that this implementation is not thread-safe.
+If your program has multiple goroutines concurrently trying to access GetInstance(),
+there is a possibility of race conditions leading to unexpected behavior
+
+To make the singleton implementation thread-safe, you should use synchronization mechanisms such as mutexes.
+Here's an example of how you can modify your implementation to make it thread-safe:
+
+package main
+
+import "sync"
+
+	type Singleton interface {
+		AddOne() int
+	}
+
+	type singleton struct {
+		count int
+	}
+
+var (
+
+	instance     *singleton
+	instanceOnce sync.Once
+
+)
+
+	func GetInstance() Singleton {
+		instanceOnce.Do(func() {
+			instance = new(singleton)
+		})
+
+		return instance
+	}
+
+	func (s *singleton) AddOne() int {
+		s.count++
+		return s.count
+	}
+*/
 type Singleton interface {
 	AddOne() int
 }
