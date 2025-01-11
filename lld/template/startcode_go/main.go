aac873291e403aff65b7f5de3bdf33fd3116cc0c
manvirag <anubhavguptadec1999@gmail.com>
2025-01-11 11:42:34 +0530

    basic structure

---
+++ lld/template/startcode_go/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"lld/repo"
+)
+
+func main() {
+	fmt.Println("hello world")
+	repo.Play()
+
+}
---
+++ lld/template/startcode_go/repo/userrepo.go
@@ -0,0 +1,7 @@
+package repo
+
+import "fmt"
+
+func Play() {
+	fmt.Println("hello world")
+}
